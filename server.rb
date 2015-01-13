c6613c42fa28a4058e198ab88f4f60ae3575da6d
benhutchinson <ben.hutchinson.personal@gmail.com>
2015-01-13 18:01:35 +0000

    Local Files, First Push

---
+++ server.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+set :views, Proc.new { File.join(root, "/views") }
+set :public_folder, Proc.new { File.join(root, "/public") }
+
+get '/' do
+  erb :index
+end
+
+get '/barchart' do 
+  erb :barchart
+end
+
+get '/barchart2' do 
+  erb :barchart2
+end
+
+get '/barchart3' do 
+  erb :barchart3
+end
+
+get '/chap5' do
+  erb :chap5
+end
+
+get '/chap6' do
+  erb :chap6
+end
